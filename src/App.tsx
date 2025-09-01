2f8e17745945e59b4e357e13344e282da59e5a82
asukachikaru <asuka@asukawang.com>
2025-09-01 20:24:16 +0900

    feat: router base setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { HeroPage } from './pages/hero'
+import { Route, Router } from '@solidjs/router';
+import { HeroPage } from './pages/Hero';
+import { ItemPage } from './pages/Item';
+import { HomePage } from './pages/Home';
 
 function App() {
   return (
     <div class="">
-      app
-      <HeroPage />
+      <Router>
+        <Route path="/hero" component={HeroPage} />
+        <Route path="/item" component={ItemPage} />
+        <Route path="/" component={HomePage} />
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
