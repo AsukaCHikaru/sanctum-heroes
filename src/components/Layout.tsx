606df8530f5356cfbcb78b06448b981d6479cabf
asukachikaru <asuka@asukawang.com>
2025-09-15 15:29:32 +0900

    feat: header style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,12 +4,12 @@ import { children, type JSXElement } from 'solid-js';
 export const Layout = (props: { children: JSXElement }) => {
   const safeChildren = children(() => props.children);
   return (
-    <div class="m-2 h-screen max-w-[960px] mx-auto">
-      <header class="flex text-gray-50 gap-4 justify-center">
-        <A href="/">HOME</A>
-        <A href="/hero">HERO</A>
-        <A href="/item">ITEM</A>
-        <A href="/mercenary">MERCENARY</A>
+    <div class="h-screen max-w-[960px] mx-auto">
+      <header class="flex text-gray-50 gap-10 justify-center mb-6 py-4 border-b border-gray-600">
+        <HeaderLink href="/">HOME</HeaderLink>
+        <HeaderLink href="/hero">HERO</HeaderLink>
+        <HeaderLink href="/item">ITEM</HeaderLink>
+        <HeaderLink href="/mercenary">MERCENARY</HeaderLink>
       </header>
       <main>{safeChildren()}</main>
       <footer class="text-center text-gray-500 text-sm mt-16 pb-10">
@@ -18,3 +18,9 @@ export const Layout = (props: { children: JSXElement }) => {
     </div>
   );
 };
+
+const HeaderLink = ({ href, children }: { href: string; children: JSXElement }) => (
+  <A href={href} class="text-xl font-serif hover:text-amber-400">
+    {children}
+  </A>
+);
