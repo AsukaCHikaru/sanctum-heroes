73663bc68b73e1395c5e9ff91956f3c74b0e401f
asukachikaru <asuka@asukawang.com>
2025-09-15 22:16:03 +0900

    feat: hero list mobile layout

--- src/components/HeroList.tsx
+++ src/components/HeroList.tsx
@@ -19,17 +19,17 @@ const neutralCommanders = HERO.filter((hero) => hero.faction === 'neutral' && he
 export const HeroList = () => {
   return (
     <div class="w-full text-center text-amber-300">
-      <div class="grid grid-cols-3 w-full gap-4">
+      <div class="flex flex-col sm:grid sm:grid-cols-3 w-full gap-4">
         <List title="同盟軍將軍" heroes={allianceGenerals} />
         <List title="同盟軍刺客" heroes={allianceAssassins} />
         <List title="同盟軍指揮官" heroes={allianceCommanders} />
       </div>
-      <div class="grid grid-cols-3 w-full gap-4">
+      <div class="flex flex-col sm:grid sm:grid-cols-3 w-full gap-4">
         <List title="天譴軍將軍" heroes={undeadGenerals} />
         <List title="天譴軍刺客" heroes={undeadAssassins} />
         <List title="天譴軍指揮官" heroes={undeadCommanders} />
       </div>
-      <div class="grid grid-cols-3 w-full gap-4 mt-4">
+      <div class="flex flex-col sm:grid sm:grid-cols-3 w-full gap-4 mt-4">
         <List title="中立將軍" heroes={neutralGenerals} />
         <List title="中立刺客" heroes={neutralAssassins} />
         <List title="中立指揮官" heroes={neutralCommanders} />
@@ -38,19 +38,17 @@ export const HeroList = () => {
   );
 };
 
-const List = ({ title, heroes }: { title: string; heroes: HeroData[] }) => {
-  return (
-    <div>
-      <h3 class="text-lg my-2">{title}</h3>
-      <div class="flex flex-wrap">
-        <For each={heroes}>
-          {(hero) => (
-            <A href={`/hero/${hero.title.en.toLowerCase().replace(/\s+/g, '-')}`}>
-              <IconImage src={`/assets/hero/${hero.icon}`} alt={hero.name.en} />
-            </A>
-          )}
-        </For>
-      </div>
+const List = ({ title, heroes }: { title: string; heroes: HeroData[] }) => (
+  <div>
+    <h3 class="text-lg my-2">{title}</h3>
+    <div class="flex flex-wrap justify-center">
+      <For each={heroes}>
+        {(hero) => (
+          <A href={`/hero/${hero.title.en.toLowerCase().replace(/\s+/g, '-')}`}>
+            <IconImage src={`/assets/hero/${hero.icon}`} alt={hero.name.en} />
+          </A>
+        )}
+      </For>
     </div>
-  );
-};
+  </div>
+);
